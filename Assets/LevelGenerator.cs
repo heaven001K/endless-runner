3229084929f0859b2874583f76ff9a71df12f9cc
heaven001K <148751533+heaven001K@users.noreply.github.com>
2024-10-08 22:01:49 +0300

    first commit

---
+++ Assets/LevelGenerator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    [SerializeField] private Transform[] levelPart;
+
+    [SerializeField] private Vector3 nextPartPosition;
+
+    [SerializeField] private float distanceToSpawn;
+    [SerializeField] private float distanceToDelete;
+    [SerializeField] private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    GeneratePlatform();
+    }
+
+
+    private void GeneratePlatform()
+    {   
+        while  (Vector2.Distance(player.transform.position, nextPartPosition) > distanceToSpawn) 
+        {
+            Transform part  = levelPart[Random.Range(0, levelPart.Length)];
+            Vector2 newPosition = new Vector2(nextPartPosition.x - part.Find("StartPoint").position.x, 0);
+            Transform newPart = Instantiate(part,newPosition,transform.rotation,transform);
+            nextPartPosition = newPart.Find("EndPoint").position;
+        }
+    }
+}
